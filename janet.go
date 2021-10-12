a39a2daa7a35f4fd43940a0ad28de3bba7d10962
Steven vanZyl <rushsteve12@gmail.com>
2021-10-12 22:55:54 +0000

    Evaluating Janet files
    Got it to evaluate Janet files and return the string of their last value
    This is a huge step since this is a core functionality of Spinnerette
    Non-janet files just get served directly via Go

--- janet.go
+++ janet.go
@@ -9,6 +9,16 @@ package main
 #include "janet.h"
 */
 import "C"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"unsafe"
+)
+
+func DeInit() {
+	C.janet_deinit()
+}
 
 func ParentEnv() *C.JanetTable {
 	return (*C.JanetTable)(C.NULL)
@@ -18,7 +28,40 @@ func EvalString(code string) (C.Janet, error) {
 	C.janet_init()
 	env := C.janet_core_env(ParentEnv())
 	var out C.Janet 
-	C.janet_dostring(env, C.CString(code), C.CString("spinnerette"), &out)
-	C.janet_deinit()
+	errno := C.janet_dostring(env, C.CString(code), C.CString("spinnerette"), &out)
+	if errno != 0 {
+		return C.janet_wrap_nil(), errors.New(fmt.Sprintf("Janet error: %d", errno))
+	}
 	return out, nil
+}
+
+func EvalBytes(code []byte) (C.Janet, error) {
+	C.janet_init()
+	env := C.janet_core_env(ParentEnv())
+	var out C.Janet
+	errno := C.janet_dobytes(env, (*C.uchar)(unsafe.Pointer(&code[0])), (C.int)(len(code)), C.CString("spinnerette"), &out)
+	if errno != 0 {
+		return C.janet_wrap_nil(), errors.New(fmt.Sprintf("Janet error: %d", errno))
+	}
+	return out, nil
+}
+
+func EvalFilePath(path string) (C.Janet, error) {
+	code, err := os.ReadFile(path)
+	if err != nil {
+		return C.janet_wrap_nil(), err
+	}
+
+	C.janet_init()
+	env := C.janet_core_env(ParentEnv())
+	var out C.Janet
+	errno := C.janet_dobytes(env, (*C.uchar)(unsafe.Pointer(&code[0])), (C.int)(len(code)), C.CString(path), &out)
+	if errno != 0 {
+		return C.janet_wrap_nil(), errors.New(fmt.Sprintf("Janet error: %d", errno))
+	}
+	return out, nil
+}
+
+func ToString(janet C.Janet) string {
+	return C.GoString((*C.char)(unsafe.Pointer(C.janet_to_string(janet))))
 }
--- main.go
+++ main.go
@@ -1,10 +1,74 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
+	"net/http/cgi"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+type Flags struct {
+	Root string
+	Method string
+	Port int
+}
+
+var parsedFlags Flags
+
 func main() {
-	EvalString("(print \"hi from janet\")")
-	fmt.Println("hi from go")
+	parsedFlags = ParseFlags()
+	parsedFlags.Method = strings.ToLower(parsedFlags.Method)
+
+	var handler Handler
+
+	if parsedFlags.Method == "http" {
+		log.Printf("Starting HTTP server on port %d", parsedFlags.Port)
+		addr := fmt.Sprintf(":%d", parsedFlags.Port)
+		http.ListenAndServe(addr, handler)
+	} else if parsedFlags.Method == "fastcgi" || parsedFlags.Method == "fcgi" {
+		// TODO
+	} else if parsedFlags.Method == "cgi" {
+		cgi.Serve(handler)
+	} else {
+		log.Fatal("Unknown method")
+	}
+}
+
+
+func ParseFlags() Flags {
+	var flg Flags
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	flag.StringVar(&flg.Method, "method", "http", "The method that Spinnerette will listen on (HTTP, FastCGI, or CGI")
+	flag.StringVar(&flg.Root, "root", wd, "Webroot files will be found in")
+	flag.IntVar(&flg.Port, "port", 9999, "Port to use for HTTP")
+
+	return flg
+}
+
+type Handler struct {}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := filepath.Join(parsedFlags.Root, r.URL.Path)
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		http.NotFound(w, r)
+	} else if filepath.Ext(path) == ".janet" {
+		janet, err := EvalFilePath(path)
+		defer DeInit()
+		if err != nil {
+			log.Printf(err.Error())
+		}
+		w.Write([]byte(ToString(janet)))
+	} else {
+		http.ServeFile(w, r, path)
+	}
 }
